884f9e16059d476ce7deb72034f2efcf7e4b6ada
Yauheni <kimberok@gmail.com>
2015-04-24 15:46:42 +0300

    Add Basic Code
    Add Basic Code

---
+++ Source/MOBA/MOBAGameMode.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/GameMode.h"
+#include "MOBAGameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class MOBA_API AMOBAGameMode : public AGameMode
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
