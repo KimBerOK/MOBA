53198c63a4542449561b297ca10d1edce0f61715
Yauheni <kimberok@gmail.com>
2015-05-22 20:29:32 +0300

    Add new version for movement (Part 2)
    Add new version for movement (Part 2)
    Add header code.

--- Source/MOBA/MOBACharacter.h
+++ Source/MOBA/MOBACharacter.h
@@ -13,22 +13,6 @@ protected:
 	// Called to bind functionality to input
 	virtual void SetupPlayerInputComponent(class UInputComponent* InputComponent) override;
 
-		//handles moving forward/backward
-	UFUNCTION()
-		void MoveForward(float Val);
-
-		//handles strafing
-	UFUNCTION()
-		void MoveRight(float Val);
-
-		//sets jump flag when key is pressed
-	UFUNCTION()
-		void OnStartJump();
-
-		//clears jump flag when key is released
-	UFUNCTION()
-		void OnStopJump();
-
 public:
 	// Sets default values for this character's properties
 	AMOBACharacter();
@@ -46,6 +30,60 @@ public:
 	// Constructor for AMOBACharacter
 	AMOBACharacter(const FObjectInitializer& ObjectInitializer);
 
+	/******************************
+	***         MOVEMENT        ***
+	*******************************/
+
+	// handles moving forward/backward
+	virtual void MoveForward(float Val);
+
+	// handles strafing
+	virtual void MoveRight(float Val);
+
+	/* Client mapped to Input */
+
+	void OnCrouchToggle();
+	// sets jump flag when key is pressed
+	void OnStartJump();
+	// clears jump flag when key is released
+	void OnStopJump();
+	void OnStartSprinting();
+	void OnStopSprinting();
+
+	/* Character wants to run, checked during Tick to see if allowed */
+
+	UPROPERTY(Transient, Replicated)
+		bool bWantsToRun;
+
+	/* Is character currently perfoming a jump action. Reset on landed. */
+
+	UPROPERTY(Transient, Replicated)
+		bool bIsJumping;
+	UFUNCTION(BlueprintCallable, Category = "Movement")
+		bool IsInitiatedJump() const;
+		
+		void SetIsJumping(bool NewJumping);
+
+	UFUNCTION(Reliable, Server, WithValidation)
+		void ServerSetIsJumping(bool NewJumping);
+
+		void OnLanded(const FHitResult& Hit) override;
+
+	/* Client/local call to update sprint state */
+		void SetSprinting(bool NewSprinting);
+
+	/* Server side call to update actual sprint state */
+		UFUNCTION(Server, Reliable, WithValidation)
+			void ServerSetSprinting(bool NewSprinting);
+
+		UFUNCTION(BlueprintCallable, Category = "Movement")
+			bool IsSprinting() const;
+
+			float GetSprintingSpeedModifier() const;
+
+		UPROPERTY(EditDefaultsOnly, Category = "Movement")
+			float SprintingSpeedModifier;
+
 
 	/******************************
 	***        TARGETING       ****
@@ -54,6 +92,8 @@ public:
 	/* Is player aiming down sights */
 	UFUNCTION(BlueprintCallable, Category = "Targeting")
 		bool IsTargeting() const;
+
+		float GetTargetingSpeedModifier() const;
 
 	/* ???For what???*/
 	UPROPERTY(Transient, Replicated)
